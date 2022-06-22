c0ec2706c1e505e1a7aac4f578e2f97fcb4eccbd
Nikolay <veselovnd@gmail.com>
2022-06-22 15:45:05 +0300

    small fixes

--- Stickers/src/model/Label.java
+++ Stickers/src/model/Label.java
@@ -5,10 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-
 import javax.imageio.ImageIO;
 
 public class Label extends AbstractLabel{
