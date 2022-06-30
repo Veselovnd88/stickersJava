60a70995c571eaa93c18d498023c7f64f01aadcc
Nikolay <veselovnd@gmail.com>
2022-06-30 14:50:18 +0300

    1

--- Stickers/src/view/WindowFx.java
+++ Stickers/src/view/WindowFx.java
@@ -5,7 +5,6 @@ package view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
