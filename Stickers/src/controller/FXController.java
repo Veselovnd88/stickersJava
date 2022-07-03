4ebf696cc4a26a0e10af287d6215a7e6b9e746ad
nikolay <veselovnd@gmail.com>
2022-07-03 21:47:04 +0300

    initialize

--- Stickers/src/controller/FXController.java
+++ Stickers/src/controller/FXController.java
@@ -1,18 +1,29 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import command.CommandExecutor;
 import command.Operation;
 import exception.InterruptOperationException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import model.MainModel;
 import model.Model;
 import view.ConsoleView;
 import view.GuiView;
 import view.View;
 
-public class FXController {
+public class FXController{ // implements Initializable {
 	
 	@FXML
 	private Label mainLbl;
@@ -20,6 +31,23 @@ public class FXController {
 	private Button placeButton;
 	@FXML 
 	private Button SaveButton;
+	@FXML
+	private ChoiceBox<Integer> pos_btn;
+	@FXML
+	private TextArea text_area;
+	@FXML
+	private TextField serial_area;
+	@FXML
+	private RadioButton rb_1mpa;
+	@FXML
+	private RadioButton rb_06mpa;
+	@FXML
+	private RadioButton rb_10bar;
+	@FXML
+	private RadioButton rb_6bar;
+	
+	
+	
 	
 	
 	private Controller controller;
@@ -34,21 +62,35 @@ public class FXController {
 		CommandExecutor.init(controller);
 		
 	}
+	public void init() {
+		ObservableList<Integer> positions = FXCollections.observableArrayList(1,2,3,4,5,6,7,
+				8,9,10,11,12);
+		pos_btn.setItems(positions);
+	}
 	@FXML
-	public void onMouseSaveclick() throws InterruptOperationException {
+	public void onMouseSaveClick() throws InterruptOperationException {
+		
 		controller.onSave();
 	}
 	
 	
 	@FXML
 	public void onMousePlaceClick() throws InterruptOperationException {
-		int art = chosenRadiobutton;//FIXME
+		int art =1; //1chosenRadiobutton;//FIXME
 		controller.onSetArt(art);
-		int pos = chosenPos;//FIXME
+		int pos = 1;//chosenPos;//FIXME
 		controller.onSetPos(pos);
 		
 
 		CommandExecutor.execute(Operation.CHOOSE);
+	}
+	@FXML
+	public void initialize() {
+		ObservableList<Integer> positions = FXCollections.observableArrayList(1,2,3,4,5,6,7,
+				8,9,10,11,12);
+		pos_btn.setItems(positions);
+	
+		
 	}
 	
 	
--- Stickers/src/view/WindowFx.java
+++ Stickers/src/view/WindowFx.java
@@ -1,43 +1,34 @@
 package view;
 
 
-import java.io.File;
 import java.net.URL;
 
-import controller.FXController;
 import controller.FXController;
 /*VM arguments
  * --module-path C:\Users\VeselovND\Downloads\openjfx-18.0.1_windows-x64_bin-sdk\javafx-sdk-18.0.1\lib --add-modules=javafx.controls
  * */
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class WindowFx extends Application {
-
+	@FXML 
+	private ChoiceBox<Integer> pos_btn;
 	@Override
 	public void start(final Stage stage) throws Exception {
 			FXMLLoader loader = new FXMLLoader();
 			URL url = getClass().getClassLoader().getResource("gui.fxml");
 			System.out.println(url);
 			loader = new FXMLLoader(url);
+			
 			loader.setLocation(url);
-			loader.setController(new FXController());
+			FXController fxcont = new FXController();
+		
+			loader.setController(fxcont);
 			
 
 		///Stickers/resources/guiFx.fxml
