9c9b53b0beff1c85e19183ead0604d4594f2a8b6
Nikolay <veselovnd@gmail.com>
2022-07-06 14:13:02 +0300

    testing to delete commits

--- Stickers/src/Main.java
+++ Stickers/src/Main.java
@@ -4,7 +4,7 @@ import view.WindowFx;
 public class Main {
 
 	public static void main(String[] args) {
-
+		//this is for checkin and deleting commit
 		//Controller c = new Controller();
 		//View view = new ConsoleView();
 		//View guiview = new GuiView();
